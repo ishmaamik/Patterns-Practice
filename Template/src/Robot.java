c64119b81de65971177e0c4d805a32016b4f73f0
Ishmaam Iftekhar Khan <ishmaam@iut-dhaka.edu>
2025-04-11 01:22:04 +0600

    Template pattern done

---
+++ Template/src/Robot.java
@@ -0,0 +1,28 @@
+public abstract class Robot {
+    //usually abstract when subclasses define the algorithms
+    //but concrete is fine
+    protected void start(){
+        System.out.println("System booted");
+    }
+    //protected so that the children can modify the logic
+
+    protected void move(){
+        System.out.println("Moving to coordinates");
+    }
+
+    protected void turnOff(){
+        System.out.println("Turning off");
+    }
+
+    public final void execute(){
+        start();
+        move();
+        turnOff();
+        //exactly in this order
+    }
+
+    public void upgrade(){
+        System.out.println("Upgrading sire!");
+        //can be upgraded anytime so sequence doesn't matter
+    }
+}
